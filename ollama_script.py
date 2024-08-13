3092ee76986e4b0cbc66fcf0f705165d5baa0aa8
Abishek <mariyaabishek@goldennest.ai>
2024-08-13 12:38:40 +0530

    add chatbot script using gemini and ollama model

---
+++ ollama_script.py
@@ -0,0 +1,83 @@
+
+# # memory store have two methods 
+# # 1) Using a ChatModel
+# # 2)using llm
+
+# 1) Using a ChatModel and ollama model
+from typing import List
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.messages import BaseMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.runnables import ConfigurableFieldSpec
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_ollama.llms import OllamaLLM
+
+
+class InMemoryHistory(BaseChatMessageHistory, BaseModel):
+    """In memory implementation of chat message history."""
+
+    messages: List[BaseMessage] = Field(default_factory=list)
+
+    def add_messages(self, messages: List[BaseMessage]) -> None:
+        """Add a list of messages to the store"""
+        self.messages.extend(messages)
+
+    def clear(self) -> None:
+        self.messages = []
+
+store = {}
+
+def get_session_history(
+    user_id: str, conversation_id: str
+) -> BaseChatMessageHistory:
+    if (user_id, conversation_id) not in store:
+        store[(user_id, conversation_id)] = InMemoryHistory()
+    return store[(user_id, conversation_id)]
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You're an assistant who's good at {ability}"),
+    MessagesPlaceholder(variable_name="history"),
+    ("human", "{question}"),
+])
+
+chain = prompt | OllamaLLM(model="llama3")
+
+with_message_history = RunnableWithMessageHistory(
+    chain,
+    get_session_history=get_session_history,
+    input_messages_key="question",
+    history_messages_key="history",
+    history_factory_config=[
+        ConfigurableFieldSpec(
+            id="user_id",
+            annotation=str,
+            name="User ID",
+            description="Unique identifier for the user.",
+            default="",
+            is_shared=True,
+        ),
+        ConfigurableFieldSpec(
+            id="conversation_id",
+            annotation=str,
+            name="Conversation ID",
+            description="Unique identifier for the conversation.",
+            default="",
+            is_shared=True,
+        ),
+    ],
+)
+
+while True:
+    user_message=input("User: ")
+    if user_message!="exit":
+        rseponse=with_message_history.invoke(
+            {"ability": "genereal", "question": user_message},
+            config={"configurable": {"user_id": "123", "conversation_id": "1"}}
+        )
+        print("Bot: ",rseponse)
+    else:
+        print("##conversation over...##")
+        break
+
+
